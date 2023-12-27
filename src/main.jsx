f3538bacb0e3ec968955e9f7bf9f62302785d53c
jizhang80 <zhangji80@outlook.com>
2023-12-27 09:50:33 -0500

    add pages and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//
+import ErrorPage from './pages/ErrorPage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import ResumePage from './pages/ResumePage.jsx';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
